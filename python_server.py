e0e49958946d9a926b417d0c29745d915dddb21c
MrCasco <ernesto.casco.velazquez@hotmail>
2020-06-25 18:14:13 -0500

    Update python_server.py

--- python_server.py
+++ python_server.py
@@ -2,5 +2,5 @@ from flask import Flask, request
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def result():
-    print(request.form['a']) # should display 'bar'
+    print(request.form['a']) # should display '1'
     return 'Received !' # response to your request.
