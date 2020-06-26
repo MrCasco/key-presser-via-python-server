aa6dd1ffa10d62c97734f36bdd0fdf28793fae05
Jose Angel Sanchez Castillejos <jsanchezcastillejos@gmail.com>
2020-06-25 20:31:46 -0500

    Add keypresset to separete file, stop from printing O every hit

--- python_server.py
+++ python_server.py
@@ -1,23 +1,13 @@
-# from flask import Flask, request
-# app = Flask(__name__)
-# @app.route('/', methods=['POST'])
-#
-# def result():
-#     print(request.form['a']) # should display '1'
-#     return 'Received !' # response to your request.
-#
-# while True:
-#     result()
 
-
-# Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from pynput.keyboard import Key, Controller
-import time
 
-keyboard = Controller()
-hostName = "192.168.1.81"
+from presser.key_presser import Presser
+
+keyboard = Presser()
+hostName = "0.0.0.0"
 serverPort = 8080
+
 
 class MyServer(BaseHTTPRequestHandler):
 
@@ -34,10 +24,10 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
-        letter = self.path[-1]
-        keyboard.press(letter)
-        keyboard.release(letter)
-        print('a')
+        if self.path != '/favicon.ico':
+            letter = self.path[-1]
+            keyboard.press_letter(letter)
+
 
 if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
