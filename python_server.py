ee10d3f99ad0e30c88b2465ab6801e1c4c89bc17
MrCasco <ernesto.casco.velazquez@hotmail>
2020-06-25 20:36:07 -0500

    Added the last buttons

--- python_server.py
+++ python_server.py
@@ -14,9 +14,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from pynput.keyboard import Key, Controller
 import time
+from socket import gethostname
 
 keyboard = Controller()
-hostName = "192.168.1.81"
+hostName = gethostname()
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
@@ -28,12 +29,25 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<form action='request.py'>", 'utf-8'))
+        self.wfile.write(bytes("<button value='z' type='submit' name='button'>z</button>", "utf-8"))
+        self.wfile.write(bytes("<button value='q' type='submit' name='button'>q</button>", "utf-8"))
         self.wfile.write(bytes("<button value='a' type='submit' name='button'>a</button>", "utf-8"))
         self.wfile.write(bytes("<button value='1' type='submit' name='button'>1</button>", "utf-8"))
+        self.wfile.write(bytes("<button value='esc' type='submit' name='button'>Esc</button>", "utf-8"))
+        self.wfile.write(bytes("<button value='tab' type='submit' name='button'>Tab</button>", "utf-8"))
+        self.wfile.write(bytes("<button value='|' type='submit' name='button'>|</button>", "utf-8"))
+        self.wfile.write(bytes("<button value='f1' type='submit' name='button'>f1</button>", "utf-8"))
+        self.wfile.write(bytes("<button value='f2' type='submit' name='button'>f2</button>", "utf-8"))
         self.wfile.write(bytes("</form>", "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
+        letter = self.path[-1]
+        if letter != 'o':
+            keyboard.press(letter)
+            keyboard.release(letter)
+
+    def typeLetter(self, path):
         letter = self.path[-1]
         print(letter)
         keyboard.press(letter)
